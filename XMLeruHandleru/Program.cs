b85a7df1f63e414c5d341147090e4091096e4525
Psyko31415 <anton@loof.homedns.org>
2017-01-12 22:55:01 +0100

    Add project files.

---
+++ XMLeruHandleru/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLeruHandleru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
